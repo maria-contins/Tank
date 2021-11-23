6c6580495091695c5dbcee8cadaad52197ce232b
Duda <m.contins@campus.fct.unl.pt>
2021-11-23 14:38:20 +0000

    Better version, with shooter now

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ let mode; // Drawing mode (gl.LINES or gl.TRIANGLES)
 let animation = true; // Animation is running
 let view;
 
-let VP_DISTANCE = 2;
+let VP_DISTANCE = 4;
 const TORUS_RADIUS = 0.7;
 
 // FLOOR
@@ -53,6 +53,10 @@ const FIRST_ROW_WHEELS = 0.9;
 const SECOND_ROW_WHEELS = 0.3;
 const THIRD_ROW_WHEELS = -0.3;
 const FOURTH_ROW_WHEELS = -0.9;
+
+// CANNON
+let cannonPosition;
+const CANNON_ROTATION_ANGLE = 1;
 
 // VIEWS
 const FRONT_VIEW  = lookAt([1, 0, 0], [0, 0, 0], [0, 1, 0]);
@@ -97,10 +101,14 @@ function setup(shaders) {
         rotateWheels += WHEELS_ROTATION_ANGLE;
         break;
       case "w":
+        cannonPosition += CANNON_ROTATION_ANGLE ;
+        break;
       case "W":
         mode = gl.LINES;
         break;
       case "s":
+        cannonPosition += CANNON_ROTATION_ANGLE ;
+        break;
       case "S":
         mode = gl.TRIANGLES;
         break;
@@ -128,7 +136,7 @@ function setup(shaders) {
     }
   };
 
-  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+  gl.clearColor(0.9, 0.7, 0.8, 1);
 
   SPHERE.init(gl);
   CUBE.init(gl);
@@ -215,8 +223,8 @@ function setup(shaders) {
 
   // This function draws our floor in a 20x20 grid
   function floor() {
-    for (let x = -10; x <= 10; x += 0.5) {
-      for (let z = -10; z <= 10; z += 0.5) {
+    for (let x = -10; x <= 10; x += 1) {
+      for (let z = -10; z <= 10; z += 1) {
         // We save the matrix before drawing every cube so that we don't start
         // adding up the transformations
         pushMatrix();
@@ -230,15 +238,15 @@ function setup(shaders) {
           // x and z were determined from the two for's we are in.
           // Our y is actually HALF the height of our rectangles because
           // we want the surface of our floor to be at y = 0
-          multTranslation([x, -(HEIGHT_FLOOR / 2), z]);
+          multTranslation([x, 0, z]);
 
           uploadModelView();
           // Here we (attempt to) color the grid
           let colors;
-          if ((x + z) % 2 === 0) {
-            colors = [0.6, 0.6, 0.6];
+          if ((x + z) % 2 == 0) {
+            colors = [1, 0.8, 0.9];
           } else {
-            colors = [1, 1, 1];
+            colors = [1, 0.9, 1];
           }
           activateColor(colors);
           CUBE.draw(gl, program, mode);
@@ -356,7 +364,7 @@ function setup(shaders) {
       multScale([2.5, 0.5, 1.35]);
 
       uploadModelView();
-      activateColor([1, 0, 0.5]);
+      activateColor([0.8, 0.3, 0.4]);
       CUBE.draw(gl, program, mode);
       deactivateColor();
     }
@@ -365,67 +373,128 @@ function setup(shaders) {
     // POINTY SIDES
     pushMatrix();
     {
-      multScale([0.5, 0.5/2, 1.35]);
+      multScale([0.5, 0.5/2, 1.7]);
       multRotationX(90);
       multRotationZ(90);
       multTranslation([0, 3, 0.5]);
 
       uploadModelView();
-      activateColor([1, 0, 0.5]);
+      activateColor([0.9, 0.4, 0.5]);
       PRISM.draw(gl, program, mode);
       deactivateColor();
     }
     popMatrix();
     pushMatrix();
     {
-      multScale([0.5, 0.5/2, 1.35]);
+      multScale([0.5, 0.5/2, 1.7]);
       multRotationX(-90);
       multRotationZ(90);
       multTranslation([0, 3, 0.5]);
 
       uploadModelView();
-      activateColor([1, 0, 0.5]);
+      activateColor([1, 0.5, 0.6]);
       PRISM.draw(gl, program, mode);
       deactivateColor();
     }
     popMatrix();
     pushMatrix();
     {
-      multScale([0.5, 0.5/2, 1.35]);
+      multScale([0.5, 0.5/2, 1.7]);
       multRotationX(90);
       multRotationZ(-90);
       multRotationY(180);
       multTranslation([0, 3, 0.5]);
 
       uploadModelView();
-      activateColor([1, 0, 0.5]);
+      activateColor([1, 0.5, 0.6]);
       PRISM.draw(gl, program, mode);
       deactivateColor();
     }
     popMatrix();
     pushMatrix();
     {
-      multScale([0.5, 0.5/2, 1.35]);
+      multScale([0.5, 0.5/2, 1.7]);
       multRotationX(-90);
       multRotationZ(-90);
       multRotationY(-180);
       multTranslation([0, 3, 0.5]);
 
       uploadModelView();
-      activateColor([1, 0, 0.51]);
+      activateColor([0.9, 0.4, 0.5]);
+      PRISM.draw(gl, program, mode);
+      deactivateColor();
+    }
+    popMatrix();
+    {
+      multScale([0.5, 0.5/2, 1.7]);
+      multRotationX(-90);
+      multRotationZ(-90);
+      multRotationY(-180);
+      multTranslation([0, 3, 0.5]);
+
+      uploadModelView();
+      activateColor([0.9, 0.4, 0.5]);
       PRISM.draw(gl, program, mode);
       deactivateColor();
     }
     popMatrix();
 
+
+    // WHEEL COVERS
+    pushMatrix();
+    {
+      multTranslation([0, 0.25/2, 0.85]);
+      multScale([2.5, 0.25, 0]);
+
+      uploadModelView();
+      activateColor([1, 0.5, 0.6]);
+      CUBE.draw(gl, program, mode);
+      deactivateColor();
+    }
+    popMatrix();
+    pushMatrix();
+    {
+      multTranslation([0, 0.25/2, -0.85]);
+      multScale([2.5, 0.25, 0]);
+
+      uploadModelView();
+      activateColor([1, 0.5, 0.6]);
+      CUBE.draw(gl, program, mode);
+      deactivateColor();
+    }
+    popMatrix();
+    pushMatrix();
+    {
+      multTranslation([0, -0.25/2, 0.85]);
+      multScale([2.5, 0.25, 0]);
+
+      uploadModelView();
+      activateColor([0.9, 0.4, 0.5]);
+      CUBE.draw(gl, program, mode);
+      deactivateColor();
+    }
+    popMatrix();
+    pushMatrix();
+    {
+      multTranslation([0, -0.25/2, -0.85]);
+      multScale([2.5, 0.25, 0]);
+
+      uploadModelView();
+      activateColor([0.9, 0.4, 0.5]);
+      CUBE.draw(gl, program, mode);
+      deactivateColor();
+    }
+    popMatrix();
+
+
     // SLANTED EDGES
     pushMatrix();
     {
-      multScale([2.5, 0.5, 1.35]);
+      multScale([2.5, 0.5, 1.7]);
       multTranslation([0, 1, 0]);
 
       uploadModelView();
-      activateColor([1, 0.2, 0.5]);
+      activateColor([1, 0.55, 0.65]);
       PYRAMID.draw(gl, program, mode);
       deactivateColor();
     }
@@ -435,15 +504,43 @@ function setup(shaders) {
     pushMatrix();
     {
       multScale([2, 0.25 , 1]);
-      multTranslation([0, 2- 0.1, 0])
+      multTranslation([0, 2 - 0.1, 0])
 
       uploadModelView();
-      activateColor([1, 0.25, 0.6]);
+      activateColor([1, 0.6, 0.7]);
+      CUBE.draw(gl, program, mode);
+      deactivateColor();
+    }
+    popMatrix();
+    pushMatrix();
+    {
+      multTranslation([0, 0.6, 0]);
+      multScale([2, 0.009 , 1]);
+
+
+      uploadModelView();
+      activateColor([1, 0.65, 0.75]);
+      CUBE.draw(gl, program, mode);
+      deactivateColor();
+    }
+    popMatrix();
+    pushMatrix();
+    {
+      multTranslation([1+0.009, 0.5-0.01, 0]);
+      multScale([0.009, 0.25 , 1]);
+
+
+      uploadModelView();
+      activateColor([1, 0.7, 0.8]);
       CUBE.draw(gl, program, mode);
       deactivateColor();
     }
     popMatrix();
 
+    top();
+  }
+
+  function top() {
     // SHOOTER RIM
     pushMatrix();
     {
@@ -452,32 +549,117 @@ function setup(shaders) {
       multTranslation([0, 3.25, 0]);
 
       uploadModelView();
-      activateColor([0, 0, 0]);
+      activateColor([1, 0.7, 0.9]);
       CYLINDER.draw(gl, program, mode);
       deactivateColor();
     }
     popMatrix();
-
+    // CABIN
     pushMatrix();
     {
       multScale([1.05, 0.4, 0.6]);
       multTranslation([0, 2, 0]);
 
       uploadModelView();
-      activateColor([1, 0.25, 0.5]);
+      activateColor([0.9, 0.4, 0.5]);
+      CYLINDER.draw(gl, program, mode);
+      deactivateColor();
+    }
+    popMatrix();
+    pushMatrix();
+    {
+      multTranslation([0, 1+(0.009/2), 0]);
+      multScale([1.05, 0.009, 0.6]);
+
+
+      uploadModelView();
+      activateColor([0.95, 0.45, 0.55]);
       CYLINDER.draw(gl, program, mode);
       deactivateColor();
     }
     popMatrix();
 
-    // HEART
+    heart();
+
+    cannon();
+  }
+
+  function cannon() {
+    pushMatrix(); // BASE
+    {
+      multTranslation([0.5, 0.8, 0]);
+      multRotationX(90);
+      multRotationZ(90);
+      multScale([0.15, 0.5, 0.15]);
+
+      uploadModelView();
+      activateColor([0.8, 0.3, 0.4]);
+      CYLINDER.draw(gl, program, mode);
+      deactivateColor();
+    }
+    popMatrix();
+    pushMatrix(); // SHADOW BASE
+    {
+      multTranslation([0.75+(0.009/2), 0.8, 0]);
+      multRotationX(90);
+      multRotationZ(90);
+      multScale([0.15, 0.009, 0.15]);
+
+      uploadModelView();
+      activateColor([0.6, 0.1, 0.2]);
+      CYLINDER.draw(gl, program, mode);
+      deactivateColor();
+    }
+    popMatrix();
+    pushMatrix();  // PIPE
+    {
+      multTranslation([1, 0.8, 0]);
+      multRotationX(90);
+      multRotationZ(90);
+      multScale([0.1, 1.5, 0.1]);
+
+      uploadModelView();
+      activateColor([0.7, 0.2, 0.3]);
+      CYLINDER.draw(gl, program, mode);
+      deactivateColor();
+    }
+    popMatrix();
+    pushMatrix();  // MUZZLE
+    {
+      multTranslation([1.5 + (0.25/2), 0.8, 0]);
+      multRotationX(90);
+      multRotationZ(90);
+      multScale([0.125, 0.25, 0.125]);
+
+      uploadModelView();
+      activateColor([0.8, 0.3, 0.4]);
+      CYLINDER.draw(gl, program, mode);
+      deactivateColor();
+    }
+    popMatrix();
+    pushMatrix(); // HOLE
+    {
+      multTranslation([1.75 + (0.009/2), 0.8, 0]);
+      multRotationX(90);
+      multRotationZ(90);
+      multScale([0.08, 0.009, 0.08]);
+
+      uploadModelView();
+      activateColor([0.2, 0.2, 0.2]);
+      CYLINDER.draw(gl, program, mode);
+      deactivateColor();
+    }
+    popMatrix();
+  }
+
+  function heart() {
     pushMatrix();
     {
       multScale([0.15, 0.05, 0.15]);
       multTranslation([-0.5, 10.25*2, 0.5]);
 
       uploadModelView();
-      activateColor([0, 0, 0]);
+      activateColor([1, 0.7, 0.9]);
       CYLINDER.draw(gl, program, mode);
       deactivateColor();
     }
@@ -488,7 +670,7 @@ function setup(shaders) {
       multTranslation([-0.5, 10.25*2, -0.5]);
 
       uploadModelView();
-      activateColor([0, 0, 0]);
+      activateColor([1, 0.7, 0.9]);
       CYLINDER.draw(gl, program, mode);
       deactivateColor();
     }
@@ -500,12 +682,11 @@ function setup(shaders) {
       multTranslation([0, 10.25*2, 0]);
 
       uploadModelView();
-      activateColor([0, 0, 0]);
+      activateColor([1, 0.7, 0.9]);
       CUBE.draw(gl, program, mode);
       deactivateColor();
     }
     popMatrix();
-
   }
 
 
@@ -533,8 +714,6 @@ function setup(shaders) {
     // actually we've shrunk our wheels and therefore the radius has also
     // changed.
     let debug = -((rotateWheels * Math.PI) / 180) * TORUS_RADIUS;
-
-
     // This moves our tank forward or backward, according to the distance
     // traveled and also places the base of it, aka the wheels, above the
     floor();
@@ -545,6 +724,16 @@ function setup(shaders) {
     }
     popMatrix();
     wheels();
+
+    /*pushMatrix();
+    {
+      uploadModelView();
+      activateColor([1, 0, 0]);
+      CYLINDER.draw(gl, program, mode);
+      deactivateColor();
+    }
+    popMatrix();*/
+
   }
 }
 const urls = ["shader.vert", "shader.frag"];
